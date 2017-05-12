b5bd72095db9b320ee584c68cdafc37e8ece2e90
Sam Thorogood <sam.thorogood@gmail.com>
2017-05-12 09:59:43 -0700

    iteration on lyff

--- build.sh
+++ build.sh
@@ -3,5 +3,4 @@
 emcc \
   -s WASM=1 -s ONLY_MY_CODE=1 -s EXPORTED_FUNCTIONS="['_myFunction']" \
   -g2 -O3 \
-  --separate-asm -o output.js \
-  *.c
+  -o output.js *.c
--- lyff.c
+++ lyff.c
@@ -2,12 +2,23 @@
 
 static int board[10000];
 
+extern int print(int);
+
 int main() {
   board[0] = 1;
   return 0;
 }
 
-int myFunction(int argc, char ** argv) {
+int myFunction(char *arg) {
   ++board[0];
-  return board[0];
+
+  print(100);
+  print(board[0]);
+
+  int len = 0;
+  while (arg[len]) {
+    ++len;
+  }
+
+  return len;
 }
