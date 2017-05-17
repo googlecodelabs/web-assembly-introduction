c933629f5039db3b63314b2a777641ff3d6d59f3
Sam Thorogood <sam.thorogood@gmail.com>
2017-05-16 22:09:54 -0700

    move to solution folder

--- lyff/solution/build.sh
+++ lyff/solution/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# nb. this differs from the codelab, as we compile code from the parent folder
 emcc \
-  -o output.js *.c \
+  -o output.js ../*.c \
   -s WASM=1 -s ONLY_MY_CODE=1 -s EXPORTED_FUNCTIONS="['_board_step','_board_init','_board_ref']"
